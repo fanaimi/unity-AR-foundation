4398e97a180a31e74198d7ca99d00721a50e2c21
fanaimi <fanaimi@users.noreply.github.com>
2022-01-30 18:31:48 +0000

    made plans transparent and debugging cannon balls

--- AR-foundation/Assets/Scripts/CanonBall.cs
+++ AR-foundation/Assets/Scripts/CanonBall.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 
 public class CanonBall : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision other)
     {
+        DebugManager.Instance.Echo("ball hit something");
+        RobotTouchController m_robotPlayer = other.collider.GetComponent<RobotTouchController>();
+
+        if (m_robotPlayer)
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    /*private void OnXCollisionEnter(Collision collision)
+    {
+        
         RobotTouchController m_robotPlayer = collision.collider.GetComponent<RobotTouchController>();
 
         if (m_robotPlayer)
@@ -14,5 +27,5 @@ public class CanonBall : MonoBehaviour
             Destroy(collision.gameObject);
             Destroy(gameObject);
         }
-    }
+    }*/
 }
